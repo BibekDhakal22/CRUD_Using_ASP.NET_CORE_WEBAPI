99dbb01d326ab13a530e05eef55f4ccb8898ea2f
Bibek <bibekd996@gmail.com>
2025-09-04 12:15:51 +0545

    ASP.NET Web API project added

---
+++ Models/Student.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDAppUsingASPCoreWebAPI.Models
+{
+
+
+    public class Student
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string studentName { get; set; }
+
+        [Required]
+        public string studentGender { get; set; }
+
+        [Required]
+        public int age { get; set; }
+
+        [Required]
+        public int standard { get; set; }
+
+        [Required]
+        public string fatherName { get; set; }
+       
+
+
+    }
+
+    }
+
